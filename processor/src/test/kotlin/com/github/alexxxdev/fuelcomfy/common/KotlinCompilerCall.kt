675f31e96d4625dc21b16d07621243ce7a7c7953
a.derendyaev <alexxxdev@yandex.ru>
2019-03-04 13:19:00 +0700

    fix "Could not find tools.jar"
    Signed-off-by: a.derendyaev <alexxxdev@yandex.ru>

--- processor/src/test/kotlin/com/github/alexxxdev/fuelcomfy/common/KotlinCompilerCall.kt
+++ processor/src/test/kotlin/com/github/alexxxdev/fuelcomfy/common/KotlinCompilerCall.kt
@@ -30,7 +30,7 @@ internal class KotlinCompilerCall(var scratchDir: File) {
     val services = LinkedHashMultimap.create<KClass<*>, KClass<*>>()
 
     init {
-        kaptArgs += mutableMapOf("kapt.kotlin.generated" to "./processor/build/kapt/sources")
+        kaptArgs += mutableMapOf("kapt.kotlin.generated" to "./build/kapt/sources")
     }
 
     fun addKt(path: String, source: String) {
