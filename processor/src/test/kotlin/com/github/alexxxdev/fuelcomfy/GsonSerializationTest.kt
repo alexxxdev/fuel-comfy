2ae96ae94710e00888e88046a3dfebf2c34c020e
a.derendyaev <alexxxdev@yandex.ru>
2019-03-27 13:32:32 +0700

    added tests
    Signed-off-by: a.derendyaev <alexxxdev@yandex.ru>

---
+++ processor/src/test/kotlin/com/github/alexxxdev/fuelcomfy/GsonSerializationTest.kt
@@ -0,0 +1,21 @@
+package com.github.alexxxdev.fuelcomfy
+
+import com.github.alexxxdev.fuelcomfy.common.BaseTest
+import com.github.alexxxdev.fuelcomfy.sources.GsonSerializationService
+import org.jetbrains.kotlin.cli.common.ExitCode
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+object GsonSerializationTest : BaseTest({
+    var kclass: GsonSerializationService? = null
+
+    group("Prepare class with suspend functions") {
+        val resultGenerate = generateClass()
+        val resultCompile = compileClass()
+        kclass = LoadClass<GsonSerializationService>()
+        test("Generate success") { assertEquals(ExitCode.OK, resultGenerate.exitCode) }
+        test("Compile success") { assertTrue(resultCompile) }
+        test("Load success") { assertNotNull(kclass) }
+    }
+})
